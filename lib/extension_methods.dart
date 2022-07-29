e5912207aadbf713f8722fe66cb9610496190188
imikieb <mikie.business99@gmail.com>
2022-07-29 14:49:46 -0300

    created color extension method

---
+++ lib/extension_methods.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+extension ColorSchemeExtension on ColorScheme {
+  Color get primaryColor => const Color(0xFF00A070);
+  Color get secondaryColor => const Color(0xFFDDEAEC);
+  Color get tertiaryColor => const Color(0xFF566066);
+  Color get backgroundColor => const Color(0xFF00A070);
+}
