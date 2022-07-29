d8a98a573c73d270d18f3dd318df44150286aba7
imikieb <mikie.business99@gmail.com>
2022-07-29 13:12:14 -0300

    added runApp

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:animated_floating_action_button/features/home/presentation/home.page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animated Floating Action Button',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+      },
+    );
+  }
+}
